55319d4c798a350d6c2f9609974ea6561f925228
critiqjo <john.ch.fr@gmail.com>
2016-07-06 18:32:02 +0530

    bb2-rs: merging done

--- bb2-rs/src/main.rs
+++ bb2-rs/src/main.rs
@@ -11,12 +11,57 @@ pub mod schema;
 
 use diesel::prelude::*; // alot of traits + diesel::result::*
 use diesel::pg::PgConnection;
+use merge2d::Point;
+use std::iter::Peekable;
 
 #[derive(Debug, Queryable)]
 pub struct DevMaxParams {
     max_load_cc: Option<i64>,
     max_poh: Option<i64>,
     total: i64,
+}
+
+pub struct StepItem {
+    key: i64,
+    val: Point<i64, i64>,
+}
+
+pub struct StepIter<I: Iterator> {
+    inner: Peekable<I>,
+}
+
+impl<I> StepIter<I>
+    where I: Iterator<Item=StepItem>
+{
+    pub fn new(iter: I) -> StepIter<I> {
+        StepIter {
+            inner: iter.peekable(),
+        }
+    }
+
+    pub fn next_step<'a>(&'a mut self) -> Option<(i64, ParamIter<'a, I>)> {
+        self.inner.peek().map(|s| s.key)
+                         .map(move |p| (p, ParamIter { param: p, inner: &mut self.inner }))
+    }
+}
+
+pub struct ParamIter<'a, I: Iterator + 'a> {
+    param: i64,
+    inner: &'a mut Peekable<I>,
+}
+
+impl<'a, I> Iterator for ParamIter<'a, I>
+    where I: Iterator<Item=StepItem> + 'a
+{
+    type Item = StepItem;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(true) = self.inner.peek().map(|s| s.key).map(|p| p == self.param) {
+            self.inner.next()
+        } else {
+            None
+        }
+    }
 }
 
 fn main() {
@@ -31,11 +76,21 @@ fn main() {
                        .filter(max_load_cc.is_not_null())
                        .group_by((max_load_cc, max_poh))
                        .order((max_load_cc, max_poh))
-                       .limit(5);
+                       .limit(100);
         //print_sql!(e);
         e.load::<DevMaxParams>(&con).expect("Error loading posts!")
-    };
-    for d in results {
-        println!("{:?}", d);
+    }.into_iter().map(|dmp| StepItem {
+                    key: dmp.max_load_cc.unwrap(),
+                    val: Point { k: dmp.max_poh.unwrap(), v: dmp.total },
+                });
+    let mut points: Vec<(i64, Vec<Point<i64, i64>>)> = Vec::new();
+    let mut steps = StepIter::new(results);
+    if let Some((p, pi)) = steps.next_step() {
+        points.push((p, pi.map(|item| item.val).collect()));
     }
+    while let Some((p, pi)) = steps.next_step() {
+        let last_new = merge2d::merge(&points.last().unwrap().1, pi.map(|item| item.val));
+        points.push((p, last_new));
+    }
+    println!("{:#?}", points.last().unwrap());
 }
